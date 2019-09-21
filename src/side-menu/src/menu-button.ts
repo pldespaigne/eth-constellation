539244f7d148d6c067efddf18c5df75018d62ef9
pldespaigne <pl.despaigne@gmail.com>
2019-09-21 17:38:40 +0200

    add button

---
+++ src/side-menu/src/menu-button.ts
@@ -0,0 +1,78 @@
+import {
+  html,
+  LitElement,
+  customElement,
+  property,
+} from "lit-element";
+
+const theme = {
+  light: '#78909c',
+  base: 'none',
+  dark: '#37474f'
+}
+
+@customElement('menu-button')
+export class MenuButton extends LitElement {
+
+  @property( { type : String }  ) icon = 'library_add';
+  @property( { type : Boolean }  ) selected = false;
+
+  background: string;
+  transform: string;
+
+  constructor() {
+    super();
+    this.background = theme.base;
+    this.transform = 'none';
+  }
+
+  mouseOver() {
+    this.background = theme.light;
+    this.requestUpdate();
+  }
+  mouseOut() {
+    this.background = theme.base;
+    this.transform = 'none';
+    this.requestUpdate();
+  }
+  mouseDown() {
+    this.background = theme.dark;
+    this.transform = 'translateY(3px)';
+    this.requestUpdate();
+  }
+  mouseUp() {
+    this.background = theme.light;
+    this.transform = 'none';
+    this.requestUpdate();
+    // const click = new Event('click');
+    // this.dispatchEvent(click);
+  }
+
+  public render() {
+    return html`
+      <button
+        @mouseover="${this.mouseOver}"
+        @mouseout="${this.mouseOut}"
+        @mousedown="${this.mouseDown}"
+        @mouseup="${this.mouseUp}"
+        style="
+          width: 40px;
+          height: 40px;
+          padding: 0;
+          border: none;
+          margin: 10px;
+          border-radius: 5px;
+          background: ${this.selected ? theme.light : this.background};
+          transform: ${this.transform};
+          display: block;
+          cursor: pointer;
+        "
+      >
+        <img
+          style="width: 40px; height: 40px"
+          src="https://fonts.gstatic.com/s/i/materialicons/${this.icon}/v1/24px.svg"
+        />
+      </button>
+    `;
+  }
+}
--- src/side-menu/src/side-menu.ts
+++ src/side-menu/src/side-menu.ts
@@ -2,18 +2,28 @@
 import {
   LitElement,
   html,
-  customElement
+  customElement,
+  property
 } from 'lit-element'
 
 import { PluginClient, connectIframe } from '@remixproject/plugin';
+
+import './menu-button';
+
+interface ButtonData {
+  icon: string,
+  name: string,
+}
 
 @customElement('side-menu')
 export class SideMenu extends LitElement {
 
   plugin: PluginClient;
+  buttons: ButtonData[];
 
   constructor() {
     super();
+    this.buttons = [];
     this.plugin = new PluginClient();
     this.plugin.onload(() => {
       this.plugin.call('app' as any, 'setStyle', 'height: 100%; width: 60px;');
@@ -21,16 +31,30 @@ export class SideMenu extends LitElement {
     connectIframe(this.plugin);
   }
 
+  handleClick(name: string) {
+    console.log(`You have clicked on ${name}`);
+  }
+
   public render() {
     return html`
       <div
         style="
-          height: 100%;
+          min-height: 100%;
           width: 100%;
-          background-color: #708090;
+          background-color: #546e7a;
+          overflow-x: hidden;
         "
       >
-        Hello World
+        <div>
+          <button @click="${() => {this.buttons.push({icon: 'stars', name: 'plugin'+this.buttons.length}); this.requestUpdate();}}">+</button>
+          <button @click="${() => {this.buttons.pop(); this.requestUpdate();}}">-</button>
+        </div>
+        ${this.buttons.map(button => 
+          html`<menu-button
+            @click="${() => {this.handleClick(button.name)}}"
+            icon="${button.icon}"
+          ></menu-button>`)
+        }
       </div>
     `;
   }
